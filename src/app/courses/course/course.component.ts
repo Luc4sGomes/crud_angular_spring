ad7d4434af5297230455ac72ba3f5c6a10aca896
Lucas Gomes <lucasgominhaa448@gmail.com>
2024-01-18 12:29:20 -0300

    added catch errors and matDialogs

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-course',
@@ -15,8 +17,20 @@ export class CourseComponent implements OnInit {
 
   //coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.listCourses();
+  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
+    this.courses$ = this.coursesService.listCourses().pipe(
+      catchError(error => {
+        this.onError('Error to Load Courses.')
+        return [];
+      })
+    );
+  }
+
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
